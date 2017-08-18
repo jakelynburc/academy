3cb980159e25c7dcc7f051a1cd1426741808f4ac
jakelynbur <jakelyn_burciaga@hotmail.com>
2017-08-18 09:32:24 -0700

    viernes

--- MyDemoProyect/MyDemoProyect public webside/scripts/catalog.js
+++ MyDemoProyect/MyDemoProyect public webside/scripts/catalog.js
@@ -1,40 +1,94 @@
-﻿//Create a JSON Object that contains my products information.
+﻿//Create a JSON Object my products information
 
-//Ready: After HTML loads
 $(document).ready(function () {
-    //$("#DynamicHTML").append(buildMyProduct(MyCatalogData));
-    GetAllProducts();
+
+   // $("#DynamicHTML").append(buildMyProduct(MyCatalogData));
+    $("#DynamicHTML").append(buildMyProduct());
+
+    //LoadMySimpleJson();
+
 });
 
-//Dynamic build your collection
-//function buildMyProduct(obj) {
-//    var html = "";
-//    $.each(obj, function (element, object) {
-//        html += "<div class='Catalog ProducItem'>";
-//        html += "<div><a href='details.html?id=" + object.id + "'>" + object.title + "</a></div>";
-//        html += "<div><img class='Catalog' src='" + object.imgUrl + "' /></div>";
-//        html += "<div>" + object.description + "</div></div>";
-//    });
-//    return html;
-//}
-function GetAllProducts() {
+//GetAllProducts
+
+function buildMyProduct() {
+
+    var html = "";
+
     $.ajax({
+
         type: 'GET',
+
         url: 'http://localhost:51184/api/product',
+
         contentType: "application/json; charset=utf-8",
+
         dataType: "json",
+
         success: function (data) {
-            var html = "";
-                $.each(data, function (element, object) {
-                    html += "<div class='Catalog ProducItem'>";
-                    html += "<div><a href='details2.html?id=" + object.Id + "'>" + object.Description + "</a></div>";
-                    html += "<div><img class='Catalog' src='" + object.imgUrl + "' /></div>";
-                    html += "<div>" + object.Price + "</div></div>";
-                });
-                $("#DynamicHTML").append(html);
+
+            $.each(data, function (element, object) {
+
+                html += "<div class='col-md-4 thumbnail'>";
+
+                html += "<div><a href='Detail2.html?id=" + object.$id + "'>" + object.Description + "</a></div>";
+
+                html += "<div><img class='Catalog' style='width:260px; height:180px' src='" + object.ImagURL + "' /></div>";
+
+                html += "<div>Model: " + object.Model + "</div>";
+
+                html += "<div>Price: " + object.Price + "</div></div>";
+
+            });
+
+            $("#IMAGE").append(html);
+
         },
+
         error: function (a, b, c) {
 
+
+
         }
+
     });
+
 }
+
+////Dynamic build your collection
+
+////function buildMyProduct(JSONCatalog) {
+
+////    var html = "";
+
+////    $.each(JSONCatalog, function (element, object) {
+
+////        html += "<div class='col-md-4 thumbnail'>";
+
+////        html += "<div><a href='Detail2.html?id=" + object.id + "'>" + object.title + "</a></div>";
+
+////        html += "<div><img class='Catalog' style='width:260px; height:180px' src='" + object.imgUrl + "' /></div>";
+
+////        html += "<div>" + object.description + "</div></div>";
+
+////    });
+
+////    return html;
+
+////}
+
+
+
+////ReadingSimpleJSON
+
+////function LoadMySimpleJson()
+
+////{
+
+////    var myJSON = mySimpleJson;
+
+//////    $.each(mySimpleJson, function (element, object) {
+
+//////});
+
+////}
